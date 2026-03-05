a2672e234172a97f8749f2e7e0f411658b000a3e
Jose-Hernandez-45 <jh733325@gamil.com>
2026-03-05 07:41:46 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Encino_cam',
+  webDir: 'dist'
+};
+
+export default config;
